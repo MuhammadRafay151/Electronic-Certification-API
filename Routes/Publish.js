f2179968b06f87eb176f9955f404c2b96152a6d2
Muhammad Rafay <muhammadrafay151@gmail.com>
2021-06-13 23:51:21 +0500

    single refactored without testing

--- Routes/Publish.js
+++ Routes/Publish.js
@@ -16,51 +16,11 @@ const LogHandler = require("../js/logsHandler");
 router.post("/single", Auth.authenticateToken, Auth.CheckAuthorization([Roles.Admin, Roles.Issuer]), async (req, res) => {
     try {
         await CountHandler.ReduceCount(req.user.org_id, 1);
-        let publish = {
-            status: true,
-            publisher_name: req.user.name,
-            publisher_email: req.user.email,
-            publish_date: Date.now(),
-            processing: false
-        }
         if (req.app.get("BlockChain_Enable")) {
-            let ct = await cert.findOneAndUpdate({ _id: req.body.id, 'issuedby.org_id': req.user.org_id, 'publish.status': false, 'publish.processing': false }, { $set: { 'publish.processing': true } }).lean()
-            if (ct) {
-                if (config.get("app.debugging") === true) {
-                    const io = req.app.get("socketio");
-                    ct.message = "send to message queue";
-                    io.to("debugging").emit("log", ct);
-                }
-                await LogHandler.Log(JSON.stringify(ct), Constants.Pending);
-                await MsgBroker.send(true, { user: req.user, certid: req.body.id })
-
-            }
-            res.send(`we are publishing your ${ct.title} certificate with id: ${ct._id}. You may continue what you are doing.`)
-
+            await PublishSingleBlockChain(req, res);
         }
         else {
-
-            try {
-                let crt = await cert.findOneAndUpdate({ _id: req.body.id, 'issuedby.org_id': req.user.org_id, 'publish.status': false }, { $set: { publish: publish } })
-                if (crt) {
-                    let message = `${crt.title} certificate with id: ${crt._id} has been published`;
-                    let message2 = `${req.user.name} has published the certificate having title: ${crt.title} & id: ${crt._id}`;
-                    await Promise.all([
-                        await NotificationHandler.NewNotification(req.user, message, Constants.Private),
-                        await NotificationHandler.NewNotification(req.user, message2, Constants.Public)
-                    ])
-                    new SocketSingleton().emitToRoom(req.user.org_id, "NotificationAlert", { count: 1 });
-                    res.status(200).send(message)
-                } else {
-                    await CountHandler.IncreaseCount(req.user.org_id, 1);
-                    res.status(404).send("certificate not found")
-                }
-            } catch (err) {
-                await CountHandler.IncreaseCount(req.user.org_id, 1);
-                console.log(err)
-                res.status(500).send(err)
-            }
-
+            await PublishSingleDB(req, res)
         }
     } catch (err) {
         if (err instanceof StatusCodeException) {
@@ -132,4 +92,67 @@ router.post("/batch", Auth.authenticateToken, Auth.CheckAuthorization([Roles.Adm
     }
 })
 //blockchain exception raised count roll back implementation remaining
+async function PublishSingleBlockChain(req, res) {
+    let ct = null;
+    try {
+        ct = await cert.findOneAndUpdate({ _id: req.body.id, 'issuedby.org_id': req.user.org_id, 'publish.status': false, 'publish.processing': false }, { $set: { 'publish.processing': true } }).lean()
+    }
+    catch (err) {
+        await CountHandler.IncreaseCount(req.user.org_id, 1);
+        return res.status(500).send(err)
+    }
+    if (ct) {
+        if (config.get("app.debugging") === true) {
+            const io = req.app.get("socketio");
+            ct.message = "send to message queue";
+            io.to("debugging").emit("log", ct);
+        }
+        try {
+            await LogHandler.Log(JSON.stringify(ct), Constants.Pending);
+        } catch (err) {
+            console.log(err)
+        }
+        try {
+            await MsgBroker.send(true, { user: req.user, certid: req.body.id })
+            return res.send(`we are publishing your ${ct.title} certificate with id: ${ct._id}. You may continue what you are doing.`)
+        } catch (err) {
+            await CountHandler.IncreaseCount(req.user.org_id, 1);
+            return res.status(500).send("Error in connecting with the message broker")
+        }
+
+    } else {
+        await CountHandler.IncreaseCount(req.user.org_id, 1);
+        return res.status(404).send("certificate not found")
+    }
+
+}
+async function PublishSingleDB(req, res) {
+    let crt = null;
+    let publish = {
+        status: true,
+        publisher_name: req.user.name,
+        publisher_email: req.user.email,
+        publish_date: Date.now(),
+        processing: false
+    }
+    try {
+        crt = await cert.findOneAndUpdate({ _id: req.body.id, 'issuedby.org_id': req.user.org_id, 'publish.status': false }, { $set: { publish: publish } })
+    } catch (err) {
+        await CountHandler.IncreaseCount(req.user.org_id, 1);
+        return res.status(500).send(err)
+    }
+    if (crt) {
+        let message = `${crt.title} certificate with id: ${crt._id} has been published`;
+        let message2 = `${req.user.name} has published the certificate having title: ${crt.title} & id: ${crt._id}`;
+        await Promise.all([
+            await NotificationHandler.NewNotification(req.user, message, Constants.Private),
+            await NotificationHandler.NewNotification(req.user, message2, Constants.Public)
+        ])
+        new SocketSingleton().emitToRoom(req.user.org_id, "NotificationAlert", { count: 1 });
+        return res.status(200).send(message)
+    } else {
+        await CountHandler.IncreaseCount(req.user.org_id, 1);
+        return res.status(404).send("certificate not found")
+    }
+}
 module.exports = router
