41692604a7c2bceb551792712535f1f2b009aa5f
Muhammad Rafay <46870378+MuhammadRafay151@users.noreply.github.com>
2021-08-19 20:48:06 +0500

    Update BlockChainCert.js

--- js/BlockChainCert.js
+++ js/BlockChainCert.js
@@ -9,7 +9,7 @@ async function GetBlockChainCert(crt, publish) {
         expiry_date: crt.expiry_date,
         instructor_name: crt.instructor_name,
         template_id: crt.template_id,
-        default_template: cert.default_template,
+        default_template: crt.default_template,
         template: crt.template,
         publish: publish,
         logo: crt.logo,
